ad1c9d663447613c4e049223b34261d492c5e735
furufuru0319 <15j5128@stu.meisei-u.ac.jp>
2018-07-26 22:21:05 +0900

    修正

--- src/framework/InfoGame.java
+++ src/framework/InfoGame.java
@@ -1,10 +1,14 @@
 package framework;
 
 /**
- * ゲームのターン・ラウンド数を保持するクラスです。	
+ * ゲームの状態とターン・ラウンド数を保持するクラスです。
  * @author 柴田航平
  */
 public class InfoGame {
+	/**
+	 * ゲームの状態を表すフィールドです。
+	 */
+	private int gameStatus;
 	/**
 	 * ターン数を表すフィールドです。
 	 */
@@ -15,12 +19,21 @@ public class InfoGame {
 	private int roundNum;
 
 	/**
+	 * ゲームのステータスを設定するメソッドです。
+	 * @param num ターン数
+	 */
+	void setGameStatus(int num) {
+		gameStatus = num;
+	}
+
+	/**
 	 * ターン数を設定するメソッドです。
 	 * @param num ターン数
 	 */
 	void setTurnNum(int num) {
 		turnNum = num;
 	}
+
 	/**
 	 * ラウンド数を設定するメソッドです。
 	 * @param num ラウンド数
@@ -28,13 +41,23 @@ public class InfoGame {
 	void setRoundNum(int num) {
 		roundNum = num;
 	}
+
+	/**
+	 * ゲームのステータスを取得するメソッドです。
+	 * @return ゲームステータス
+	 */
+	public int getGameStatus() {
+		return gameStatus;
+	}
+
 	/**
 	 * ターン数を取得するメソッドです。
 	 * @return ターン数
 	 */
-	public int getTurnNum()  {
+	public int getTurnNum() {
 		return turnNum;
 	}
+
 	/**
 	 * ラウンド数を取得するメソッドです。
 	 * @return ラウンド数
